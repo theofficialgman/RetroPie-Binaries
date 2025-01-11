2197472ace922c06cbce194a31a8a20787807034
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2025-01-11 18:11:37 +0000

    Retropie cores update - Sat Jan 11 18:11:35 UTC 2025

--- scripts/cron_run.sh
+++ scripts/cron_run.sh
@@ -2,6 +2,6 @@
 
 # check, if sudo is used
 if [[ "$(id -u)" -ne 0 ]]; then
-    sudo "/home/ubuntu/retropie-local-binaries/update_retropie.sh"
+    sudo "/home/ubuntu/RetroPie-Binaries/scripts/update_retropie.sh"
 fi
 
